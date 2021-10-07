772617c120666ff516cca8e67effde90f7f2282c
pkflunr <71987718+pkflunr@users.noreply.github.com>
2021-10-07 00:46:48 -0700

    Implemented shooting. Added a PlayerTester scene for inheritance purposes.
    The PlayerTester scene is necessary as the shooting code is written with inheritance in mind. Updated the bullet slightly, rotating the sprite as needed. The player can now shoot bullets by pressing or holding down the mouse button.

--- United We Stand/Assets/Scenes/Player.gd
+++ United We Stand/Assets/Scenes/Player.gd
@@ -1,20 +1,26 @@
 extends KinematicBody2D
 
-var speed = 250
-var velocity = Vector2()
+export var speed = 250 # the speed at which the player moves
+var velocity = Vector2() # holds the direction the player is currently moving
+
+export (PackedScene) var bullet # reference to the bullet the player fires
+
+var shotReady = true # true when the player can fire another shot
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	get_input()
+	_get_input()
 	move_and_collide(velocity*delta)
 	
+	_shootingHandler()
+	
 
-func get_input():
+func _get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("Up"):
 		velocity.y -= 1
@@ -25,3 +31,16 @@ func get_input():
 	if Input.is_action_pressed("Left"):
 		velocity.x -= 1
 	velocity = velocity.normalized() * speed
+
+# all code related to shooting handled in this function
+func _shootingHandler():
+	if Input.is_action_pressed("Shoot") and shotReady: # when the player is trying to shoot and cd is down
+		shotReady = false # put bullet on cooldown
+		$ShotCooldown.start()
+		var newBullet = bullet.instance() # instance a bullet and place it at the fire point
+		newBullet.transform = $FirePoint.get_global_transform()
+		owner.add_child(newBullet)
+
+
+func _on_ShotCooldown_timeout():
+	shotReady = true
--- United We Stand/Assets/Scenes/PlayerBullet.gd
+++ United We Stand/Assets/Scenes/PlayerBullet.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 
 # Declare member variables here.
-export var velocity = 500
+export var velocity = 700
 var direction # Vector2 containing the direction the bullet should fly 
 
 
@@ -11,9 +11,10 @@ var direction # Vector2 containing the direction the bullet should fly
 func _ready():
 	# set the bullet's fly direction to where the mouse is at time of bullet instance
 	direction = position.direction_to(get_global_mouse_position())
+	$Sprite.rotate(atan(direction.y / direction.x))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	# make the bullet fly constantly in one direction
-	position += direction + velocity + delta
+	position += direction * velocity * delta
