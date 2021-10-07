2b20095d1fc217a0409812360cb2398ed36ae6fa
pkflunr <71987718+pkflunr@users.noreply.github.com>
2021-10-07 00:07:12 -0700

    Added bullet; renamed PlayerTest scene to Player
    The bullet flies towards the position of the mouse at the moment it was instanced. I'm just going to assume the PlayerTest scene can end up being our final player scene so I renamed it.

---
+++ United We Stand/Assets/Scenes/PlayerBullet.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Declare member variables here.
+export var velocity = 500
+var direction # Vector2 containing the direction the bullet should fly 
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# set the bullet's fly direction to where the mouse is at time of bullet instance
+	direction = position.direction_to(get_global_mouse_position())
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	# make the bullet fly constantly in one direction
+	position += direction + velocity + delta
