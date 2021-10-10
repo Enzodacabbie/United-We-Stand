47dd6045f9906cc20ac99d84f120c3658db20159
pkflunr <71987718+pkflunr@users.noreply.github.com>
2021-10-10 13:24:21 -0700

    Added an EnemyBase
    EnemyBase is a base scene that behaves as an extremely simple enemy, with a hitbox, sprite, and an HP value, which will die if that HP value is reduced to zero.

---
+++ United We Stand/Assets/Scenes/EnemyBase.gd
@@ -0,0 +1,25 @@
+extends RigidBody2D
+# Declare member variables here.
+export var maxHP = 5
+var _HP
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_HP = maxHP
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	_tryDying()
+
+# This function is intended to be called by other entities.
+# Decrements the enemy's current HP by amount (negative values will heal)
+func takeDamage(var amount):
+	_HP = clamp(_HP - amount, 0, maxHP)
+
+func _tryDying():
+	if (_HP == 0):
+		print("An enemy has been slain.")
+		queue_free()
+
